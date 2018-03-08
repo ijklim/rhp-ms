927e80e43f47235b7cad9757f2adc15837bca629
Ivan Lim <aimprove@gmail.com>
2018-03-08 15:13:05 -0500

    Extract header info

--- api/whoami.js
+++ api/whoami.js
@@ -1,8 +1,11 @@
 module.exports = function (req) {
   console.log(req.headers);
+  let ipaddress = req.headers['x-forwarded-for'].split(',')[0];
+  let language = req.headers['accept-language'].split(',')[0];
+  let software = req.headers['user-agent'].match(/\(([^\)]+)\)/)[1];
   return {
-    ipaddress: req.headers['host'],
-    language: req.headers['accept-language'],
-    software: req.headers['user-agent']
+    ipaddress,
+    language,
+    software
   };
 }
